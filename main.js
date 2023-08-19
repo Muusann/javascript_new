0a53b3cc5971e2d5f726ce44da388dd6c3abf6d3
kawakamiyohei <yamameto1994@gmail.com>
2023-08-19 11:23:10 +0900

    #03 textContentを使ってみよう

--- main.js
+++ main.js
@@ -2,8 +2,13 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
-    console.log('clicked');
+    // console.log('clicked');
+    // document.querySelector('p').textContent = 'こんにちは';
+
+    document.querySelector('p').textContent = document.querySelector('button').textContent;
   });
 
-  console.log('test');
+
+
+  // console.log('test');
 }
